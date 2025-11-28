4128295f03770407b45ff1e690ce0725694e7bb2
Thomas <sempurai95@gmail.com>
2025-11-28 13:45:45 +0800

    Update audio_toggle.js

--- scripts/audio_toggle.js
+++ scripts/audio_toggle.js
@@ -1,7 +1,10 @@
 AFRAME.registerComponent('audio-toggle', {
   schema: {
     target: { type: 'string' },    // id of AUDIO ENTITY, not <audio>
-    mode:   { default: 'pause' }   // 'pause' or 'restart'
+    mode:   { default: 'pause' },   // 'pause' or 'restart'
+    iconPlay:  { default: 'icons/play.png' },
+    iconPause: { default: 'icons/pause.png' },
+    iconStop:  { default: 'icons/stop.png' }
   },
 
   init: function () {
@@ -12,6 +15,19 @@ AFRAME.registerComponent('audio-toggle', {
       return;
     }
 
+    // Create icon element
+    this.icon = document.createElement('a-image');
+    this.icon.setAttribute('position', '0 0 0.6'); 
+    this.icon.setAttribute('scale', '0.4 0.4 0.4');
+    this.el.appendChild(this.icon);
+
+    // Set initial icon
+    if (this.data.mode === 'pause') {
+      this.icon.setAttribute('src', this.data.iconPlay);
+    } else {
+      this.icon.setAttribute('src', this.data.iconStop);
+    }
+    
     // Ensure sound component exists
     this.el.addEventListener('click', () => {
       const sound = this.targetEl.components.sound;
@@ -21,8 +37,10 @@ AFRAME.registerComponent('audio-toggle', {
       if (this.data.mode === 'pause') {
         if (sound.isPlaying) {
           sound.pauseSound();
+          this.icon.setAttribute('src', this.data.iconPlay);
         } else {
           sound.playSound();
+          this.icon.setAttribute('src', this.data.iconPause);
         }
       }
 
@@ -30,8 +48,10 @@ AFRAME.registerComponent('audio-toggle', {
       if (this.data.mode === 'restart') {
         if (sound.isPlaying) {
           sound.stopSound();
+          this.icon.setAttribute('src', this.data.iconPlay);  // Now in "ready to start" state
         } else {
           sound.playSound();
+          this.icon.setAttribute('src', this.data.iconStop);  // Show stop icon while playing
         }
       }
     });
