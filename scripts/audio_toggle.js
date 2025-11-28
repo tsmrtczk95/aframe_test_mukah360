0059e65e1aac93bd695c1f3f1e7be0175782e2e0
Thomas <sempurai95@gmail.com>
2025-11-28 13:54:15 +0800

    Update audio_toggle.js

--- scripts/audio_toggle.js
+++ scripts/audio_toggle.js
@@ -2,9 +2,6 @@ AFRAME.registerComponent('audio-toggle', {
   schema: {
     target: { type: 'string' },    // id of AUDIO ENTITY, not <audio>
     mode:   { default: 'pause' },   // 'pause' or 'restart'
-    iconPlay:  { default: 'icons/play.png' },
-    iconPause: { default: 'icons/pause.png' },
-    iconStop:  { default: 'icons/stop.png' }
   },
 
   init: function () {
@@ -13,19 +10,6 @@ AFRAME.registerComponent('audio-toggle', {
     if (!this.targetEl) {
       console.error("audio-toggle: Target entity not found:", this.data.target);
       return;
-    }
-
-    // Create icon element
-    this.icon = document.createElement('a-image');
-    this.icon.setAttribute('position', '0 0 0.6'); 
-    this.icon.setAttribute('scale', '0.4 0.4 0.4');
-    this.el.appendChild(this.icon);
-
-    // Set initial icon
-    if (this.data.mode === 'pause') {
-      this.icon.setAttribute('src', this.data.iconPlay);
-    } else {
-      this.icon.setAttribute('src', this.data.iconStop);
     }
     
     // Ensure sound component exists
@@ -37,10 +21,8 @@ AFRAME.registerComponent('audio-toggle', {
       if (this.data.mode === 'pause') {
         if (sound.isPlaying) {
           sound.pauseSound();
-          this.icon.setAttribute('src', this.data.iconPlay);
         } else {
           sound.playSound();
-          this.icon.setAttribute('src', this.data.iconPause);
         }
       }
 
@@ -48,10 +30,8 @@ AFRAME.registerComponent('audio-toggle', {
       if (this.data.mode === 'restart') {
         if (sound.isPlaying) {
           sound.stopSound();
-          this.icon.setAttribute('src', this.data.iconPlay);  // Now in "ready to start" state
         } else {
           sound.playSound();
-          this.icon.setAttribute('src', this.data.iconStop);  // Show stop icon while playing
         }
       }
     });
